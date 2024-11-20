bd25c1d9bccb04f95d05e25e2dad7e4f33ec61f6
Alaina-Noel <alaina.noel.8@gmail.com>
2024-11-19 23:21:51 -0500

    add create user command

---
+++ app/Console/Commands/CreateUser.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class CreateUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:create-user {username} {password} {email}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new user with username, password, and email';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        // Retrieve arguments
+        $username = $this->argument('username');
+        $password = $this->argument('password');
+        $email = $this->argument('email');
+
+        // Create a new user
+        $user = User::create([
+            'name' => $username,
+            'email' => $email,
+            'password' => Hash::make($password),
+        ]);
+
+        if ($user) {
+            $this->info('User successfully created.');
+        } else {
+            $this->error('Failed to create user.');
+        }
+    }
+}
